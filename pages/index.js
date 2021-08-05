669228df8f4039c3491de57c615ed58730fc2b4b
Jiachi Liu <inbox@huozhi.im>
2021-08-05 13:01:58 +0800

    init

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var jsx_runtime_1 = require("react/jsx-runtime");
+function Styled() {
+    return (jsx_runtime_1.jsx("p", { children: jsx_runtime_1.jsx("style", __assign({ jsx: true }, { children: "p {color:red}" }), void 0) }, void 0));
+}
+exports.default = Styled;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Styled() {
+  return (
+    <p>
+      <style jsx>{`p {color:red}`}</style>
+    </p>
+  )
+} 
